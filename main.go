43cff4365232172d5b89bdd6962d7d94979390c0
Adesoye AbdulMumeen <abdulmumeenadesoye19@gmail.com>
2024-05-08 08:02:02 -0700

    Printing strings and variables

--- main.go
+++ main.go
@@ -1,50 +1,30 @@
 package main
 
-import (
-	"fmt"
-
-	"rsc.io/quote"
-)
+import "fmt"
 
 func main() {
+	age:=24
+	name:="Wale"
 
-	fmt.Println("Strings")
+	//Print
+	fmt.Print("Hello, ")
+	fmt.Print("World \n")
+	fmt.Print("New line \n")
 
-	var nameOne string = "first method for writing strings"
-	var nameTwo = "Second method for writing strings"
-	var nameThree string
+	//Println
+	fmt.Println("Hello, World")
+	fmt.Println("New Line")
 
-	fmt.Println("Hello World, ", nameOne, ", ", nameTwo)
+	fmt.Println("my age is", age, "and my name is", name)
 
-	nameThree = "Declared buh just used"
-	nameOne = "updated"
+	//Printfn(formatted strings) %_ = format specifiers
+	fmt.Printf("my age is %v, and my name is %v \n", age, name)
+	fmt.Printf("my age is %q, and my name is %q \n", age, name)
+	fmt.Printf("age is of type %T \n", age)
+	fmt.Printf("you scored %f points \n", 225.55)
+	fmt.Printf("you scored %0.1f points \n", 225.55)
 
-	nameFour := "another method"
-	fmt.Println("Hello World, ",nameOne, ", " ,nameTwo,  ", " ,nameThree,  ", ",nameFour)
-
-
-	fmt.Println("Integers")
-
-	var ageOne int = 29
-	var ageTwo = 40
-	ageThree:=27
-
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	fmt.Println("bits/memory")
-
-	var numOne int8 = 25
-	var numTwo int8 = -128
-	var numThree uint8 = 255
-
-	fmt.Println(numOne, numTwo, numThree)
-
-	fmt.Println("float")
-	var floatOne float32 = -1.5
-	var floatTwo float64 = 567737832832823832.7
-	floatThree:=23.6
-
-	fmt.Println(floatOne, floatTwo, floatThree)
-
-	fmt.Println(quote.Go())
+	//Sprint (save formatted strings)
+	var str = fmt.Sprintf("my age is %v, and my name is %v \n", age, name)
+	fmt.Printf("The saved string is: %v", str)
 }
