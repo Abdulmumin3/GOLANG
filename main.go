6fab3d3ba1696947acc24d21c4d83bc42bea8d96
Adesoye AbdulMumeen <abdulmumeenadesoye19@gmail.com>
2024-05-10 09:46:36 -0700

    maps

--- main.go
+++ main.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 )
 
-var score = 99.5
-
-
 func main() {
 
-	sayHello("mario")
-
-	for _,v:=range points{
-		fmt.Println(v)
+	menu:=map[string]float64{
+		"soup": 789.6,
+		"garri": 678.9,
+		"epa": 456.7,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["epa"])
+
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	phoneBook:=map[int]string{
+		8020736247: "Wale",
+		9078965756: "Shade",
+		9197654567: "Tayo",
+	}
+
+	fmt.Println(phoneBook)
+	fmt.Println(phoneBook[8020736247])
 
 }
