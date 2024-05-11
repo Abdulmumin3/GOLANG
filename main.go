ebba7c0fc78a1cac6379a29482d54337dc26fca1
Adesoye AbdulMumeen <abdulmumeenadesoye19@gmail.com>
2024-05-11 02:01:59 -0700

    structs

--- main.go
+++ main.go
@@ -4,26 +4,9 @@ import (
 	"fmt"
 )
 
-func updateName(x *string) {
-	*x="wedge"
-}
 
 func main() {
+	myBil:=newBill("Wale's bill")
 
-	name:="tifa"
-
-	// updateName(name)
-
-	// fmt.Println("memory address of name is",&name)
-	
-	m:=&name
-	// fmt.Println("memory address:",m)
-	// fmt.Println("value at memory address:",*m)
-
-	fmt.Println(name)
-
-	updateName(m)
-
-	fmt.Println(name)
-
+	fmt.Println(myBil)
 }
