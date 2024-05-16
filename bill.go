cda50906ba41070c298517e8753f12d2e71ce71a
Adesoye AbdulMumeen <abdulmumeenadesoye19@gmail.com>
2024-05-15 23:56:59 -0700

    receiver-functions-with-pointers

--- bill.go
+++ bill.go
@@ -11,15 +11,16 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{"pie": 7.68, "gala": 11.23},
+		items: map[string]float64{},
 		tip:   0,
 	}
 
 	return b
 }
 
-func (b bill) format() string {
+func (b *bill) format() string {
 	fs := "Bill breakdown \n"
+	fs += fmt.Sprintf("%v \n", b.name)
 
 	var total float64 = 0
 
@@ -29,8 +30,21 @@ func (b bill) format() string {
 		total += v
 	}
 
+	// add tip
+	fs += fmt.Sprintf("%-25v ...$%0.2f \n", "tip:", b.tip)
+
 	// total
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f \n", "total:", total+b.tip)
 
 	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64){
+	b.tip = tip
+}
+
+// add an item to the bill
+func (b bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 func main() {
 	myBill:=newBill("Wale's bill")
 
+	myBill.addItem("onion soup", 4.50)
+	myBill.addItem("veg pie", 8.95)
+	myBill.addItem("toffee pudding", 4.95)
+	myBill.addItem("coffee", 3.25)
+
+	myBill.updateTip(10)
+
 	fmt.Println(myBill.format())
 }
