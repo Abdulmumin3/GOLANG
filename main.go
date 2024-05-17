0afa982e9e7162df85af20291149cdf80106c8b0
Adesoye AbdulMumeen <abdulmumeenadesoye19@gmail.com>
2024-05-16 21:25:21 -0700

    parsing-floats

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -30,15 +31,38 @@ func promptOptions(b bill) {
 
 	opt, _ := getInput("Choose Option: (a - add item), (s - save bill), (t - add tip): ", reader)
 	switch opt{
+
 	case "a":
 		name, _ := getInput("Item name: ", reader)
 		price, _ := getInput("Item price: ", reader)
-		fmt.Println(name, price)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("the price must be a number")
+			promptOptions(b)
+		}
+
+		b.addItem(name, p)
+		fmt.Println("Item has been added - ", name, price)
+
+		promptOptions(b)
+
 	case "t":
 		tip, _ := getInput("Enter tip amount ($): ", reader)
-		fmt.Println(tip)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("the tip must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+		fmt.Println("tip added - ", tip)
+
+		promptOptions(b)
+
 	case "s":
-		fmt.Println("you chose s")
+		fmt.Println("you chose to save the bill ", b)
+
 	default:
 		fmt.Println("That's not a valid option")
 		promptOptions(b)
